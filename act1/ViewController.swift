0cbcb93ec225104deb33e3101e2b03e4e2306ae8
manoj <manoj.bobby796@gmail.com>
2019-10-28 22:29:18 -0400

    Initial Commit

---
+++ act1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  act1
+//
+//  Created by Manoj kumar on 2019-10-28.
+//  Copyright © 2019 manoj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
